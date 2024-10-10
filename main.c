796302318a85aa5a21dea0142f60192610e9da86
Arthur Maximus <arthurmcb321@gmail.com>
2024-10-09 22:51:37 -0300

    fix: corrente & nomes

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void solveEquation(double a1, double b1, double c1, double a2, double b2, double
     A1 = D1 / D;
     A2 = D2 / D;
 
-    // Exibir as soluções
+    // Exibir as soluções (caso subamortecido)
     if (sub) {
         B1 = A1;
         B2 = A2;
@@ -34,6 +34,7 @@ void solveEquation(double a1, double b1, double c1, double a2, double b2, double
         return;
     }
 
+    // Soluções A1 e A2 para os demais casos
     printf("A1 = %.2f, ", A1);
     printf("A2 = %.2f\n", A2);
     return;
@@ -55,39 +56,46 @@ int main(){
     double tm, vtm;
     int unidade = 0;
 
-   
+    // Créditos
+
+    printf("\n");
+    printf("\033[1;92mPROGRAMA CRIADO POR: \n");
+    printf("Arthur Maximus Cavalcante Beuttenmuller\n");
+    printf("Eliabe Rodrigues Pereira\033[m\n");
+    printf("\n");
+  
     // Resistencia, Capacitancia, Indutância  
 
     printf("Digite o valor da Resistencia: ");
     scanf("%lf", &resistencia);
-    printf("Indique a unidade (1 - Ohm - 2 - nOhm, 3 - microOhm, 4 - mOhm, 5 - kOhm): \n");
+    printf("Indique a unidade (1 - Ohm - 2 - nOhm, 3 - microOhm, 4 - mOhm, 5 - kOhm): ");
     scanf("%d", &unidade);
     resistencia *= valores[unidade-1];
 
-    printf("Digite o valor da Capacitancia: ");
+    printf("\nDigite o valor da Capacitancia: ");
     scanf("%lf", &capacitancia);
-    printf("Indique a unidade (1 - F, 2 - nF, 3 - microF, 4 - mF, 5 - kF): \n");
+    printf("Indique a unidade (1 - F, 2 - nF, 3 - microF, 4 - mF, 5 - kF): ");
     scanf("%d", &unidade);
     capacitancia *= valores[unidade-1];
     
-    printf("Digite o valor da Indutancia: ");
+    printf("\nDigite o valor da Indutancia: ");
     scanf("%lf", &indutancia);
-    printf("Indique a unidade (1 - H, 2 - nH, 3 - microH, 4 - mH, 5 - kH): \n");
+    printf("Indique a unidade (1 - H, 2 - nH, 3 - microH, 4 - mH, 5 - kH): ");
     scanf("%d", &unidade);
     indutancia *= valores[unidade-1];
 
     
     // Condições Iniciais
 
-    printf("Qual o valor da Tensao Inicial no capacitor? ");
+    printf("\nQual o valor da Tensao Inicial no capacitor? ");
     scanf("%lf", &tensaoC);
-    printf("Indique a unidade (1 - V, 2 - nV, 3 - microV, 4 - mV, 5 - kV): \n");
+    printf("Indique a unidade (1 - V, 2 - nV, 3 - microV, 4 - mV, 5 - kV): ");
     scanf("%d", &unidade);
     tensaoC *= valores[unidade-1];
     
-    printf("Qual o valor da Corrente Inicial no indutor? ");
+    printf("\nQual o valor da Corrente Inicial no indutor? ");
     scanf("%lf", &correnteI);
-    printf("Indique a unidade (1 - A, 2 - nA, 3 - microA, 4 - mA, 5 - kA): \n");
+    printf("Indique a unidade (1 - A, 2 - nA, 3 - microA, 4 - mA, 5 - kA): ");
     scanf("%d", &unidade);
     correnteI *= valores[unidade-1];
 
@@ -96,9 +104,9 @@ int main(){
 
     double Ir = 0, Ic = 0;
 
-    Ir = (tensaoC/resistencia);
+    Ir = (tensaoC/resistencia); // Corrente no resistor
 
-    Ic = - (correnteI + Ir);
+    Ic = (correnteI - Ir); // Corrente no capacitor seguindo o circuito pré-determinado
     
 
     // Cálculo de Sigma e Omega
@@ -107,6 +115,8 @@ int main(){
 
     omega = (1 / sqrt(capacitancia * indutancia));
 
+
+    printf("\n\033[1;92mResultados da resposta natural de Circuitos RLC PARALELO\n\033[m"); // Cor verde
 
     // Circuito Superamortecido
 
@@ -121,12 +131,12 @@ int main(){
         printf("Sigma: %.2lf s^-1\n", sigma);
         printf("Omega: %.2lf rad/s\n", omega);
         printf("s1: %.2lf, s2: %.2lf\n", s1, s2);
-        solveEquation(1, 1, tensaoC, s1, s2, Ic/capacitancia, 0);
+        solveEquation(1, 1, tensaoC, s1, s2, Ic/capacitancia, 0); // Resolvendo o sistema 2x2
 
-        tm = (log(fabs(s1*A1)) - log(fabs(s2*A2)))/(s2 - s1);
+        tm = (log(fabs(s1*A1)) - log(fabs(s2*A2)))/(s2 - s1); // Fórmula para tm do Circuito Superamortecido
         printf("tm: %.10lf s\n", tm);
 
-        vtm = A1*exp(s1*tm) + A2*exp(s2*tm);
+        vtm = A1*exp(s1*tm) + A2*exp(s2*tm); // Substituição de tm em v(t) para descobrir o vtm
         printf("vtm: %.2lf V\n", vtm);
 
         return 0;
@@ -138,12 +148,12 @@ int main(){
         printf("\nCircuito Criticamente amortecido\n");
         printf("Sigma: %.2lf s^-1\n", sigma);
         printf("Omega: %.2lf rad/s\n", omega);
-        solveEquation(0, 1, tensaoC, 1, -1*(sigma), Ic/capacitancia, 0);
+        solveEquation(0, 1, tensaoC, 1, -1*(sigma), Ic/capacitancia, 0); // Resolvendo o sistema 2x2
                 
-        tm = ((A2/A1*sigma) - 1)/(-1*(sigma));
+        tm = ((A2/A1*sigma) - 1)/(-1*(sigma)); // Fórmula para tm do Circuito Superamortecido
         printf("tm: %.10lf s\n", tm);
 
-        vtm = (A1*tm + A2) * exp(-1 * sigma * tm);
+        vtm = (A1*tm + A2) * exp(-1 * sigma * tm); // Substituição de tm em v(t) para descobrir o vtm
         printf("vtm: %.2lf V\n", vtm);
 
         return 0;
@@ -157,12 +167,12 @@ int main(){
     printf("Sigma: %.2lf s^-1\n", sigma);
     printf("Omega: %.2lf rad/s\n", omega);
     printf("Omegad: %.2lf rad/s\n", omegad);
-    solveEquation(1, 0, tensaoC, -1*(sigma), omegad, Ic/capacitancia, 1);
+    solveEquation(1, 0, tensaoC, -1*(sigma), omegad, Ic/capacitancia, 1); // Resolvendo o sistema 2x2
 
-    tm = (atan((B2 * omegad - B1 * sigma) / (B1 * omegad + B2 * sigma)))/omegad;
+    tm = (atan((B2 * omegad - B1 * sigma) / (B1 * omegad + B2 * sigma)))/omegad; // Fórmula para tm do Circuito Superamortecido
     printf("tm: %.10lf s\n", tm);
 
-    vtm = exp( -1 * sigma * tm) * (B1 * cos(omegad * tm) + B2 * sin(omegad * tm)); 
+    vtm = exp( -1 * sigma * tm) * (B1 * cos(omegad * tm) + B2 * sin(omegad * tm));  // Substituição de tm em v(t) para descobrir o vtm
     printf("vtm: %.2lf V\n", vtm);
 
     return 0;
